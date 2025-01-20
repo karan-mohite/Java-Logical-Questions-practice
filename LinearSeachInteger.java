7127f513724deffb47ee62e51daa010c12509fbb
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-20 11:23:59 +0530

    .

---
+++ LinearSeachInteger.java
@@ -0,0 +1,13 @@
+public class LinearSeachInteger {
+        public static void main(String[] args) {
+            int a[]={5,3,6,1,4,2}; 
+            for(int i=0;i<a.length;i++)
+             {
+                if(a[i]==item)
+                {
+                    System.out.print("Element Present at index:"i+"index postion");
+                }
+             }
+        }   
+    }
+    
