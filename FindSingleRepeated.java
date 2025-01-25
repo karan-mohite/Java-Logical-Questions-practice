a6f05e9bf7bd3df50e090419fce5f53ef0e4265e
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-25 11:10:12 +0530

    .

--- FindSingleRepeated.java
+++ FindSingleRepeated.java
@@ -1,6 +1,6 @@
 public class FindSingleRepeated {
     public static void main(String[] args) {
-        int a[] = { 5, 3, 4, 5, 2, 3, 4 };
+        int a[] = { 5, 3, 4, 5, 2,2,1, 3, 4 };
 
         int res = a[0];
         for (int i = 1; i < a.length; i++) {
